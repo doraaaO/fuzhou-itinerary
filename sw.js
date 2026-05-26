7d5e230d4d0e2644beaf2fbedd7a8fd1497da89f
doraaaO <qzsb6dqswx@privaterelay.appleid.com>
2026-05-26 10:32:37 +0800

    PWA

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'fuzhou-itinerary-v1';
+const urlsToCache = [
+  './fuzhou-itinerary.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cache hit or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
